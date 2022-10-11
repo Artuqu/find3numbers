9fd1e5cbdf6866b52ed7f3665f18142f07f4345c
Artuqu <percikartur@gmail.com>
2022-10-11 20:36:51 +0200

    Add two exceptions

--- src/main/java/findNumbers.java
+++ src/main/java/findNumbers.java
@@ -7,19 +7,27 @@ public class findNumbers {
         Arrays.sort(numbers);
         boolean isInArray = false;
         int counter = 0;
-        for (int i = counter + 1; i < numbers.length; i++) {
-            int first = numbers[counter];
-            int second = numbers[i];
-            for (int j = i + 1; j < numbers.length; j++) {
-                int third = numbers[j];
-                if (first + second + third == k) {
-                    isInArray = true;
-                    i = numbers.length - 1;
+        if (numbers.length < 3) {
+            System.out.println("Array is to short!");
+        } else {
+            for (int i = counter + 1; i < numbers.length; i++) {
+                int first = numbers[counter];
+                int second = numbers[i];
+                if (first > k) {
+                    System.out.println("First value is too big");
+                    return isInArray;
                 }
-            }
-            if (i == numbers.length - 2) {
-                counter++;
-                i = counter;
+                for (int j = i + 1; j < numbers.length; j++) {
+                    int third = numbers[j];
+                    if (first + second + third == k) {
+                        isInArray = true;
+                        i = numbers.length - 1;
+                    }
+                }
+                if (i == numbers.length - 2) {
+                    counter++;
+                    i = counter;
+                }
             }
         }
 
@@ -30,10 +38,11 @@ public class findNumbers {
 
     public static void main(String[] args) {
         int[] toFind = {8, 2, 6, 9, 10, 100, 80, 33, 4, 6, 0, 20};
-        int k = 6;
+        int[] toFind2 = {8, 2, 3, 7, 9};
+        int k = 2;
 
 
-        find3numbers(toFind, k);
+        find3numbers(toFind2, k);
 
     }
 
