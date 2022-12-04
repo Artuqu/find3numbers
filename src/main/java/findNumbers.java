e61e10910c95c9098236f6558e882a05ae1faaea
Artuqu <percikartur@gmail.com>
2022-12-04 11:41:56 +0100

    Loop improved

--- src/main/java/findNumbers.java
+++ src/main/java/findNumbers.java
@@ -6,27 +6,26 @@ public class findNumbers {
     static boolean find3numbers(int[] numbers, int k) {
         Arrays.sort(numbers);
         boolean isInArray = false;
-        int counter = 0;
         if (numbers.length < 3) {
             System.out.println("Array is to short!");
+        } else if (numbers[0] > k) {
+            System.out.println("Value K is too small!");
         } else {
-            for (int i = counter + 1; i < numbers.length; i++) {
-                int first = numbers[counter];
-                int second = numbers[i];
-                if (first > k) {
-                    System.out.println("First value is too big");
-                    return isInArray;
-                }
-                for (int j = i + 1; j < numbers.length; j++) {
-                    int third = numbers[j];
-                    if (first + second + third == k) {
-                        isInArray = true;
-                        i = numbers.length - 1;
+            for (int o = 0; o < numbers.length; o++) {
+                for (int i = o + 1; i < numbers.length; i++) {
+                    int first = numbers[o];
+                    int second = numbers[i];
+                    for (int j = i + 1; j < numbers.length; j++) {
+                        int third = numbers[j];
+                        if (first + second + third == k) {
+                            isInArray = true;
+                            break;
+                        }
                     }
-                }
-                if (i == numbers.length - 2) {
-                    counter++;
-                    i = counter;
+                    if (i == numbers.length - 2) {
+                        o++;
+                        i = o;
+                    }
                 }
             }
         }
@@ -39,10 +38,11 @@ public class findNumbers {
     public static void main(String[] args) {
         int[] toFind = {8, 2, 6, 9, 10, 100, 80, 33, 4, 6, 0, 20};
         int[] toFind2 = {8, 2, 3, 7, 9};
-        int k = 2;
+        int[] toFind3 = {7, 9};
+        int k = -1;
 
 
-        find3numbers(toFind2, k);
+        find3numbers(toFind, k);
 
     }
 
