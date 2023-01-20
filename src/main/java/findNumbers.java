8a0f8246f339d20ef235d7e80402896feae510c0
Artuqu <percikartur@gmail.com>
2023-01-20 21:11:25 +0100

    Bug with loop fixed

--- src/main/java/findNumbers.java
+++ src/main/java/findNumbers.java
@@ -8,12 +8,14 @@ public class findNumbers {
         boolean isInArray = false;
         if (numbers.length < 3) {
             System.out.println("Array is to short!");
+            return false;
         } else if (numbers[0] > k) {
             System.out.println("Value K is too small!");
+            return false;
         } else {
-            for (int o = 0; o < numbers.length; o++) {
-                for (int i = o + 1; i < numbers.length; i++) {
-                    int first = numbers[o];
+            for (int o = 0; o < numbers.length - 2; o++) {
+                int first = numbers[o];
+                for (int i = o + 1; i < numbers.length - 1; i++) {
                     int second = numbers[i];
                     for (int j = i + 1; j < numbers.length; j++) {
                         int third = numbers[j];
@@ -22,14 +24,9 @@ public class findNumbers {
                             break;
                         }
                     }
-                    if (i == numbers.length - 2) {
-                        o++;
-                        i = o;
-                    }
                 }
             }
         }
-
         System.out.println(isInArray);
         return isInArray;
     }
@@ -39,10 +36,10 @@ public class findNumbers {
         int[] toFind = {8, 2, 6, 9, 10, 100, 80, 33, 4, 6, 0, 20};
         int[] toFind2 = {8, 2, 3, 7, 9};
         int[] toFind3 = {7, 9};
-        int k = -1;
+        int k = 13;
 
 
-        find3numbers(toFind, k);
+        find3numbers(toFind2, k);
 
     }
 
